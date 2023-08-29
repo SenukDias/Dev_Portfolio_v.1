79207c8e8897e1fc51740a10d4103c605c06846b
senukronin <senukronin@gmail.com>
2023-08-29 21:43:45 +0530

    new

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -177,6 +177,6 @@ Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
 Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 
-🤫 There are some Easter Eggs here !!!
+🤫 There are some Easter Eggs here !!
 `;
 };
